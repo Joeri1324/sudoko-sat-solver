05992417188319792386b1b92c2409e7438663d0
mas <mbakhtiariz@gmail.com>
2017-09-24 12:01:55 +0200

    added a mean list to keep the mean of computing time for every experiment with differen number of givens(from 1 to 80). ploted a curve for mean and givens.

--- ComputationAnalyzer.py
+++ ComputationAnalyzer.py
@@ -2,19 +2,25 @@ import SudokuIO as io
 import time
 import os
 import SudokuSolver as ss
+import matplotlib.pyplot as plt
 
 def main():
 
+    max_given_num = 80
+    samples_num = 3
     path = "/home/mas/Desktop/sudoko-sat-solver/dataset/"
     path_sol = "/home/mas/Desktop/sudoko-sat-solver/solversolution/"
     path_converted_sol = "/home/mas/Desktop/sudoko-sat-solver/convertedsol/"
     time_file_path = path_sol + "compute_time.txt"
-    time_file = open(time_file_path, "w")
+
 
     solver = ss.SudokuSolver()
     compute_time = []
-    for n in range(5,6):
-        for i in range(0,3):
+    mean = [0]*max_given_num
+
+    for n in range(1,max_given_num+1):
+        time_file = open(time_file_path, "w")
+        for i in range(0,samples_num):
             #define pathes for reading from dataset and writing to the files:
             if not os.path.exists(path + str(n) + "/"):
                 os.mkdir(path + str(n))
@@ -39,15 +45,23 @@ def main():
             time_file.write(str(time_object))
             time_file.write(" ")
 
+            mean[n - 1] = mean[n - 1] + (end_time - start_time)
+
             #saving our predicted solutions to file
             print("i:",i,"solution:",solution,'\n')
             io.save_solution(writefile, solution)
             io.save_solution_without_index(converted_sol, solution)
 
+        mean[n - 1] = (mean[n - 1])/(samples_num*1.0)
+
         time_file.write('\n')
         time_file.close()
-        print(compute_time)
-
+    print(compute_time)
+    print("mean: ",mean)
+    plt.figure(1)
+    x = [i for i in range(1,max_given_num+1)]
+    plt.plot(x,mean)
+    plt.show()
 
 if __name__ == "__main__":
     main()
