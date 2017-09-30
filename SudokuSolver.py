6773d69abd7151b999841ffc63f64e9a32c764d6
mas <mbakhtiariz@gmail.com>
2017-09-30 18:53:43 +0200

    added verbose=1 to the solve() function of pycosat to give statistics.

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -2,9 +2,10 @@ import itertools
 import sys
 import math
 import pycosat
-import os
+import sys
 import time
-import SudokuIO
+import subprocess
+import SudokuIO as io
 
 class SudokuSolver(object):
 
@@ -92,68 +93,23 @@ class SudokuSolver(object):
         ))
 
     def solve(self, sudoku):
-        return [x for x in pycosat.solve(self.get_rules(9) + sudoku) if x > 0]
+        sudo_out = [x for x in pycosat.solve(self.get_rules(9) + sudoku, verbose=1) if x > 0]
 
-
-
+        return sudo_out
 
 def main():
+    stat_file = "/home/mas/Desktop/sudoko-sat-solver/solversolution/statistic.txt"
+
+    sudoku = [[219], [714], [328], [425], [821], [331], [836], [938], [441],
+              [843], [554], [655], [757], [265], [667], [277], [579], [772],
+              [383], [486], [198]]
+
 
     solver = SudokuSolver()
-    path = "/home/mas/Desktop/sudoko-sat-solver/dataset/"
-    path_sol = "/home/mas/Desktop/sudoko-sat-solver/solversolution/"
-    path_converted_sol = "/home/mas/Desktop/sudoko-sat-solver/convertedsol/"
-    time_file_path = path_sol + "compute_time.txt"
-    time_file = open(time_file_path, "w")
-    io = SudokuIO
-    compute_time = []
-    for n in range(5,6):
-        for i in range(0,3):
-            #define pathes for reading from dataset and writing to the files:
-            if not os.path.exists(path + str(n) + "/"):
-                os.mkdir(path + str(n))
-            if not os.path.exists(path_sol + str(n) + "/"):
-                os.mkdir(path_sol + str(n))
-            if not os.path.exists(path_converted_sol + str(n) + "/"):
-                os.mkdir(path_converted_sol + str(n))
-
-            readfile = path + str(n) + "/puzzle_" + str(i) + ".txt"
-            writefile = path_sol + str(n) + "/sat_solution_" + str(i) + ".txt"
-            converted_sol = path_converted_sol + str(n) + "/index_free_sol_" + str(i) + ".txt"
-
-            #convery the normal input from data set to a readable form for sat solver:
-            solver_input = io.dataset_parse(readfile)
-
-            #solving evey puzzle and estimate its running time and saving the time records:
-            start_time = time.time()
-            solution = solver.solve(solver_input)
-            end_time = time.time()
-            time_object = [n,i,end_time - start_time]
-            compute_time.append(time_object)
-            time_file.write(str(time_object))
-            time_file.write(" ")
-
-            #saving our predicted solutions to file
-            print("i:",i,"solution:",solution,'\n')
-            io.save_solution(writefile, solution)
-            io.save_solution_without_index(converted_sol, solution)
-
-    time_file.write('\n')
-    time_file.close()
-    print(compute_time)
-
-
-
-
-
-
-
-
-
-'''    sudoku = [[219], [714], [328], [425], [821], [331], [836], [938], [441], 
-              [843], [554], [655], [757], [265], [667], [277], [579], [772],
-              [383], [486], [198]]'''
-
+    #start_time = time.process_time()
+    solver.solve(sudoku)
+    # solve the SAT problem
+    #print(str(time.process_time() - start_time).replace('.', ','))
 
 if __name__ == "__main__":
     main()
